18b8915a32da0aa009a3e3d941fc66e7e34a5ed1
Justin Lewis Salmon <justin.lewis.salmon@gmail.com>
2018-10-15 09:55:57 +0100

    Simplify posterior calculation
    Signed-off-by: Justin Lewis Salmon <justin.lewis.salmon@gmail.com>

--- q12.py
+++ q12.py
@@ -9,18 +9,16 @@ TRUE_W = [-1.3, 0.5]
 
 
 def main():
-    # Noise variance
     alpha = 2.0
     beta = (1 / 0.2) ** 2
 
     # Generate some data using the real parameters
-    x = np.linspace(-2, 2, num=20); np.random.shuffle(x)
+    x = np.linspace(-5, 5, num=20); np.random.shuffle(x)
     # Generate the true y-values (plus some noise)
     y = f(x, TRUE_W[0], TRUE_W[1]) + np.random.normal(scale=0.3, size=x.shape)
 
     # These are the x and y axis values in W-space
-    w0 = np.linspace(-2, 2, 200)
-    w1 = np.linspace(-2, 2, 200)
+    w0, w1 = np.linspace(-2, 2, 200), np.linspace(-2, 2, 200)
 
     # Define a D-dimensional starting mean vector. Assume that the mean
     # is 0, 0 to begin with
@@ -37,6 +35,8 @@ def main():
     predictions = predict(x, mean, covariance, 6)
     plot_predictions(x, predictions, 3)
 
+    running_likelihood = np.ones((w0.shape[0], w1.shape[0]))
+
     # Start with 1 data point, progressively adding more
     for i, n in enumerate([1, 2, 20]):
 
@@ -46,11 +46,12 @@ def main():
         # Plot the likelihood
         plot_pdf(w0, w1, likelihood, 3 * i + 4)
 
+        # Calculate the running likelihood
+        running_likelihood *= likelihood
+
         # Calculate the posterior, given the new data
-        # mean, covariance, posterior = calculate_posterior(
-        #     w0, w1, x[0:n], y[0:n], mean, covariance, beta, likelihood, prior)
-        posterior = calculate_posterior(
-                w0, w1, x[0:n], y[0:n], mean, covariance, beta, likelihood, prior)
+        posterior, mean, covariance = calculate_posterior(
+                x[0:n], y[0:n], mean, covariance, beta, running_likelihood, prior)
 
         # Plot the posterior distribution
         plot_pdf(w0, w1, posterior, 3*i + 5)
@@ -95,7 +96,6 @@ def calculate_likelihood(w0, w1, x, y, beta):
         for j in range(0, n - 1):
 
             mean = f(x, w0[i], w1[j])
-
             norm_const = 1.0 / (np.sqrt(2.0 * np.pi) * (1 / beta))
             exponent = -0.5 * (y - mean) * beta * (y - mean)
 
@@ -104,51 +104,21 @@ def calculate_likelihood(w0, w1, x, y, beta):
     return pdf
 
 
-def calculate_posterior(w0, w1, x, y, mean, covariance, beta, likelihood, prior):
-    """Update the mean and covariance matrices based on the given data"""
+def calculate_posterior(x, y, mean, covariance, beta, likelihood, prior):
+    """Return the posterior distribution and updated mean and covariance"""
     phi_x = phi(x)
-    # Precision is the inverse of the covariance
     precision = np.linalg.inv(covariance)
 
-    # p31?
-    new_precision = precision + beta * phi_x.T.dot(phi_x)
-    new_mean = np.linalg.solve(
-        new_precision,
-        precision.dot(mean) + beta * phi_x.T.dot(y)
-    )
-    new_covariance = np.linalg.inv(new_precision)
+    # See lecture 4 slide 15
+    covariance = np.linalg.inv(precision + beta * phi_x.T.dot(phi_x))
+    mean = covariance.dot(precision.dot(mean) + beta * phi_x.T.dot(y))
 
-    pdf = multivariate_normal.pdf(
-        np.dstack(np.meshgrid(w0, w1)),
-        mean=new_mean,
-        cov=new_covariance
-    )
-
-    # M, N = w0.shape[0], w1.shape[0]
-    # pdf = np.zeros((M, M))
-    # S_N_inv = alpha * np.eye(2, 2) + beta * PHI.T * PHI
-    # S_N = inv(S_N_inv)
-    # m_N = beta * S_N * PHI.T * data
-    # for xt in range(0, M - 1):
-    #     for yt in range(0, M - 1):
-    #         x_v = np.array([w0[xt], w1[yt]])
-    #
-    #         norm_const = (1.0 / (2.0 * np.pi * np.sqrt(np.linalg.det(S_N))))
-    #
-    #         tmp3 = np.array([[x_v[0] - m_N[0, 0], x_v[1] - m_N[1, 0]]])
-    #         exponent = -0.5 * tmp3 * np.linalg.inv(S_N) * tmp3.T
-    #
-    #         pdf[xt, yt] = norm_const * np.exp(exponent)
-
-    return likelihood * prior
-
-    # return new_mean, new_covariance, pdf
+    return likelihood * prior, mean, covariance
 
 
 def predict(x, mean, covariance, num_samples):
     """Predict a number of y-values given some x-values"""
-    w_sample = np.random.multivariate_normal(
-        mean, covariance, size=num_samples)
+    w_sample = np.random.multivariate_normal(mean, covariance, size=num_samples)
     y = phi(x).dot(w_sample.T)
     return y
 
@@ -173,8 +143,8 @@ def plot_predictions(x, y, i):
     plt.xlabel('$\mathregular{x}$')
     plt.ylabel('$\mathregular{y}$')
     plt.plot(x, y, "-r")
-    plt.xlim(-2, 2)
-    plt.ylim(-2, 2)
+    plt.xlim(-5, 5)
+    plt.ylim(-5, 5)
 
 
 if __name__ == '__main__':
